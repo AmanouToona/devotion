e3494be85d0d711bb751ff59c644de4fef068d0b
toona <amanou.toona@gmail.com>
2020-11-01 17:47:10 +0900

    unseolved ABC160_D

--- ABC160_D.py
+++ ABC160_D.py
@@ -1,3 +1,16 @@
 # D - Line++
 import sys
 
+N, X, Y = map(int, sys.stdin.readline().strip().split())
+
+for k in range(1, N):
+    if k > N - (Y - X):
+        print('if', 0)
+    elif k < Y - X:
+        print('elif', N - k + 1 + min(X, N - Y, k))
+    else:
+        if X >= k:
+            ans = X - k + 1 + (N - Y + 1) - k + 1
+        else:
+            ans = N - (Y - X) - k + 1
+        print('else', ans)
