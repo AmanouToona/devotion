bb6d39c980d175fb450b4146f1e83017e57ca2e2
toona <amanou.toona@gmail.com>
2020-11-17 07:34:44 +0900

    learning Segment Tree

---
+++ ABC125_C.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <template T>
+struct SegmentTree {
+    private:
+        int n;
+        vector<T> node;
+    
+    public:
+        SegmentTree {
+
+        }
+}
+
+
+int main() {
+    int N;
+    vector<int> A;
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        A.push_back(a);
+    }
+}
