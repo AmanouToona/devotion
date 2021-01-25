784d543eb27a37db2abecae1ec10bed0eebd1a77
toona <amanou.toona@gmail.com>
2021-01-25 20:54:48 +0900

    solved ABC172_d

--- ABC172_D.cpp
+++ ABC172_D.cpp
@@ -3,8 +3,14 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    int N;
+    ll N;
     cin >> N;
+    
+    ll ans = 0;
+    for (int i = 1; i <=N; i++) {
+        ans += i * (N / i + 1) * (N / i) / 2;
+    }
 
+    cout << ans << endl;
     
 }
