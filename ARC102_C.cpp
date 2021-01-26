cd514465b07f9e6b15d4c5487cef3294e180fa0e
toona <amanou.toona@gmail.com>
2021-01-27 00:00:20 +0900

    solved ARC102_C

---
+++ ARC102_C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> num(K, 0);
+
+    for (int i = 1; i <= N; i++) {
+        num[i % K]++;
+    }
+
+    ll ans = 0;
+    for (int a = 0; a < K; a++) {
+        int b = (K - a) % K;
+        int c = (K - a) % K;
+
+        if ((b + c) % K == 0) {
+            ans += num[a] * num[b] * num[c]; 
+        }
+    }
+
+    cout << ans << endl;
+
+}
