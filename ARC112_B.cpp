2a97eeaebd274fce8750d816738c21bc72f323ce
toona <amanou.toona@gmail.com>
2021-02-27 20:42:45 +0900

    AC ARC112_B

--- ARC112_B.cpp
+++ ARC112_B.cpp
@@ -7,16 +7,17 @@ int main() {
     cin >> B >> C;
 
     if (C == 1) {
-        cout << 2 << endl;
+        if (B != 0) {cout << 2 << endl;}
+        else {cout << 1 << endl;}
         return 0;
     }
 
     ll ans = 0;
-    ll left1 = B - floor(C / 2);
-    ll right1 = B + floor((C - 2) / 2);
+    ll left1 = B - floorl(C / 2);
+    ll right1 = B + floorl((C - 2) / 2);
 
-    ll left2 = -B - floor((C - 1) / 2);
-    ll right2 = -B + floor((C - 1) / 2);
+    ll left2 = -B - floorl((C - 1) / 2);
+    ll right2 = -B + floorl((C - 1) / 2);
 
     if (right2 < left1 || right1 < left2) {
         ans = right1 - left1 + 1 + right2 - left2 + 1;
