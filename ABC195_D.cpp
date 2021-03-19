54bbf01c45652b7b1ffda4c3da9cb5ba58a7318d
toona <amanou.toona@gmail.com>
2021-03-19 21:04:53 +0900

    AC ABC195_D

--- ABC195_D.cpp
+++ ABC195_D.cpp
@@ -7,25 +7,43 @@ int main() {
     int N, M, Q;
     cin >> N >> M >> Q;
 
-    vector<int> W(N), V(N);
-    vector<pair<int, int>> X;
+    vector<pair<int, int>> baggage, X;
+
     for(int i = 0; i < N; i++) {
-        cin >> W[i] >> V[i];
-    };
+        int w, v;
+        cin >> w >> v;
+        baggage.push_back(make_pair(v, w));
+    }
 
     for(int i = 0; i < M; i++) {
         int x;
         cin >> x;
         X.push_back(make_pair(x, i));
     }
-    sort(X.begin(), X.end(), greater<pair<int, int>>());
+    sort(baggage.begin(), baggage.end(), greater<pair<int, int>>());
+    sort(X.begin(), X.end());
 
-    for(int i = 0; i < Q; i++) {
-        int L, R, ans=0;
+    for(int q = 0; q < Q; q++){
+        int L, R, ans = 0;
         cin >> L >> R;
-        
+        --L;
+        --R;
+    
+        vector<bool> empty(M, true);
+        for(int n = 0; n < N; n++){ 
+            for(int m = 0; m < M; m++) {
+                if(L <= X[m].second && X[m].second <= R) continue;
+                if(! empty[m]) continue;
+                
+                if(X[m].first >= baggage[n].second) {
+                    ans += baggage[n].first;
+                    empty[m] = false;
+                    break;
+                }
 
+            }
+        }
+        cout << ans << endl;
     }
-
 
 }
