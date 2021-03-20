175b846dbd5f723dbdbb3f54abdd87002c89822d
toona <amanou.toona@gmail.com>
2021-03-20 10:43:25 +0900

    AC ARC033_B

---
+++ ARC033_B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int na, nb;
+    cin >> na >> nb;
+
+    set<int> a;
+    int numerator=0, denominator=na;
+
+    for (int i = 0; i < na; i++) {
+        int ai;
+        cin >> ai;
+        a.insert(ai);
+    }
+
+    for (int i = 0; i < nb; i++) {
+        int bi;
+        cin >> bi;
+        if (a.find(bi) != a.end()) numerator++;
+        else denominator++;
+    }
+
+    double ans;
+    ans = numerator *  1.0/ denominator;
+    printf("%f11", ans);
+}
