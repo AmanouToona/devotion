b32fd22354344c53b93531cd1e3771eee88d9978
toona notepc <amanou.toona@gmail.com>
2021-03-26 08:29:11 +0900

    1WA DigitalArtsProgramingContest2012_A

---
+++ DigitalArtsProgramingContest2012_A.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    string S;
+    getline(cin, S);
+
+    vector<string> s;
+    string temp;
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i] == ' ') {
+            s.push_back(temp);
+            temp.clear();
+        } else {temp.push_back(S[i]);
+        }
+    }
+    s.push_back(temp);
+
+    int N;
+    cin >> N;
+
+    vector<string> NG(N);
+    for (int i = 0; i < N; i++) {
+        cin >> NG[i];
+    }
+
+    bool flag = true;
+    for (int word_no = 0; word_no < s.size(); word_no++) {
+        string word = s[word_no];
+        for(auto ng: NG) {
+            flag = true;
+            if (word.size() != ng.size()) {
+                flag = false;
+                continue;
+            }
+            for (int i = 0; i < word.size(); i++) {
+                if (ng[i] == '*') continue;
+                if (word[i] != ng[i]) flag = false;
+            }
+
+            if (flag) {
+                for (int i = 0; i < word.size(); i++) {
+                    cout << '*';
+                }
+                break;
+            }
+        }
+        if (!flag) {
+            cout << word;
+        }
+
+        if (word_no != s.size() - 1) {
+            cout << " ";
+        }
+    }
+    
+    cout << endl;
+
+}
