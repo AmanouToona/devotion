d967ee6fd5b9dba12378a33b9b5f8378cc6cee36
toona <amanou.toona@gmail.com>
2021-03-25 14:21:46 +0900

    4WA ARC115_C

---
+++ ARC115_C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+// sieve
+void sieve(int N, vector<int> &res) {
+    //return vector of prime numbers
+    vector<bool> prime(N + 1, true);
+
+    for (int i = 2; i < N; i++){
+        if (prime[i]) res.push_back(i);
+
+        int j = i * 2;
+        while(j <= N) {
+            prime[j] = false;
+            j += i;
+        }
+    }
+}
+
+
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> ans(N + 1, 1);
+    vector<int> prime;
+    sieve(N, prime);
+
+
+    for (auto i: prime) {
+        ll j = i;
+        while(j <= N) {
+            for (int k = j; k <= N; k+=j) {
+                ans[k]++;
+            }
+            j *= i;
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << ans[i] << " ";
+    }
+
+    cout << endl;
+}
