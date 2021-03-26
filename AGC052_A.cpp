af41748692dbaeb670a076cc30a392f68501e678
toona notepc <amanou.toona@gmail.com>
2021-03-26 17:42:41 +0900

    AC AGC052_A

---
+++ AGC052_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    for (int i = 0; i < N; i++) {
+        cout << '0';
+    }
+    for (int i = 0; i < N; i++) {
+        cout << '1';
+    }
+    cout << '0';
+    cout << endl;
+}
+
+
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        solve();
+    }
+}
