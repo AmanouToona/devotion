3adb47da0fa71ca614ef85165af98fabc5dee182
toona notepc <amanou.toona@gmail.com>
2021-05-04 09:03:27 +0900

    AC typical006

--- typical006.cpp
+++ typical006.cpp
@@ -9,17 +9,30 @@ int main() {
     string S;
     cin >> S;
 
+    // 前計算 i 文字目以降で一番左にある alpabet の位置を記録する
+    vector<vector<int>> dp(26, vector<int>(N, N));
+    for (int i = N - 1; i >= 0; i--) {
+        dp[S[i] - 'a'][i] = i;
+    }
+    for (int i = 0; i < 26; i++) {
+        for (int j = N - 1; j > 0; j--) {
+            dp[i][j - 1] = min(dp[i][j], dp[i][j - 1]);
+        }
+    }
+
+
+    // 問題を解く
     string ans = "";
-    int left = -1;
-    for (int n = 0; n < K; n++) {  // 答えのn文字目の探索
-        int min_alphabet = 30;
-        for (int i = left + 1; i < N - (K - 1 - n); i++) {
-            if (S[i] - 'a' < min_alphabet) {
-                left = i;
-                min_alphabet = S[i] - 'a';
+    int left = 0;
+    for (int k = 0; k < K; k++) {
+        for (int i = 0; i < 26; i++) {
+            int limit = N - (K - k);  // 先頭から何文字目までを用いてよいか
+            if (dp[i][left] <= limit) {
+                ans.push_back('a' + i);
+                left = dp[i][left] + 1;
+                break;
             }
         }
-        ans.push_back(S[left]);
     }
 
     cout << ans << endl;
