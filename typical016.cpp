70bbc3f92861807dfc7fc7927748d10741e5a8de
toona <amanou.toona@gmail.com>
2021-05-31 01:29:32 +0900

    AC typical016

--- typical016.cpp
+++ typical016.cpp
@@ -14,8 +14,8 @@ int main() {
     sort(Coin.rbegin(), Coin.rend());
 
     ll ans = INT_MAX;
-    for (int i = 0; N > Coin[0] * i; i++) {
-        for (int j = 0; N > Coin[0] * i + Coin[1] * j; j++) {
+    for (int i = 0; N >= Coin[0] * i; i++) {
+        for (int j = 0; N >= Coin[0] * i + Coin[1] * j; j++) {
             ll remain = N - Coin[0] * i - Coin[1] * j;
             if (remain == 0 || remain % Coin[2] == 0) {
                 ans = min<ll>(ans, i + j + remain / Coin[2]);
