2a12119fd658c92b67d21cb795c8b9aa0dab866d
toona <amanou.toona@gmail.com>
2021-07-22 00:48:08 +0900

    solving typical035 typical045

--- trial.cpp
+++ trial.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
-    int x[3][3];
+    int a = 1 << 60;
+    long long b = 1 << 60;
+    long long c = 1LL << 60;
+
+    long long d = 1;
+    d <<= 60;
+
+    printf("a: %d\nb: %lld\nc: %lld\nd: %lld", a, b, c, d);
+
 }
---
+++ typical045.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> X(N), Y(N);
+    for (int i = 0; i < N; i++) {
+        cin >> X[i] >> Y[i];
+    }
+
+
+}
