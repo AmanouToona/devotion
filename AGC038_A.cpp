831135bbca1afbbdd39366ac30d9166e53528083
toona notepc <amanou.toona@gmail.com>
2021-07-22 17:15:46 +0900

    AC AGC038_A

--- AGC038_A.cpp
+++ AGC038_A.cpp
@@ -6,67 +6,24 @@ int main() {
     int H, W, A, B;
     cin >> H >> W >> A >> B;
 
-    if (H == 1) {
-        if (A <= W && B == 0) {
-            for (int w = 0; w < W; w++) {
+    vector<vector<bool>> table(H, vector<bool>(W, false));
+
+    for (int h = 0; h < H; h++) {
+        for (int w = 0; w < W; w++) {
+            if (h < B) {
                 if (w < A) {
                     cout << 1;
                 } else {
                     cout << 0;
                 }
-            }
-            cout << endl;
-            return 0;
-        } else {
-            cout << "No" << endl;
-            return 0;
-        }
-    }
-
-    if (W == 1) {
-        if (B <= H && A == 0) {
-            for (int h = 0; h < H; h++) {
-                if (h < B) {
-                    cout << 1 << endl;
+            } else {
+                if (w < A) {
+                    cout << 0;
                 } else {
-                    cout << 0 << endl;
+                    cout << 1;
                 }
             }
-            return 0;
-        } else {
-            cout << "No" << endl;
-            return 0;
         }
+        cout << endl;
     }
-
-    vector<vector<bool>> table(H, vector<bool>(W, false));  // 0, 1 を記録
-    for (int d = 0; d < B; d++) {
-        for (int w = 0; w < W; w++) {
-            int h = (w + d) % H;
-            table[h][w] = true;  // 1: true に変更
-        }
-    }
-
-    int a = 0, b = 0;
-    for (int h = 0; h < H; h++) {
-        if (table[h][0]) b++;
-    }
-    for (int w = 0; w < W; w++) {
-        if (table[0][w]) a++;
-    }
-
-    a = min(a, W - a);
-    b = min(b, H - b);
-
-    if (a == A && b == B) {
-        for (int h = 0; h < H; h++) {
-            for (int w = 0; w < W; w++) {
-                cout << table[h][w];
-            }
-            cout << endl;
-        }
-        return 0;
-    }
-    cout << "No" << endl;
-
 }
