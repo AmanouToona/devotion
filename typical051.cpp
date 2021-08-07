57a47fd3549a818dae18d64e5244001ed95c6e11
toona <amanou.toona@gmail.com>
2021-08-07 12:49:24 +0900

    solving typical051

---
+++ typical051.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N, K;
+    ll P;
+    cin >> N >> K >> P;
+
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    
+}
