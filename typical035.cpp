33449e4b4d3c7b84f4cd3a64e0eb8c90d88b7427
toona <amanou.toona@gmail.com>
2021-09-16 20:07:43 +0900

    AC subtask2: typical035

--- typical035.cpp
+++ typical035.cpp
@@ -3,13 +3,61 @@ using ll = long long;
 using namespace std;
 
 
-void dfs(int v, int p, vector<vector<int>> &G, vector<int> &dp) {
-    for (int u: G[v]) {
-        if (u == p) continue;
-        dfs(u, v, G, dp);
-        dp[v] += dp[u];
+struct LCA {
+    vector<int> dist;  // rootからの距離
+    vector<vector<int>> parent; // parent[k][u] := u の 2^k 先の親
+    LCA(vector<vector<int>> &G) {
+        int K = 0;
+        while ((1 << K) < G.size()) K++;
+
+        parent.assign(K, vector<int>(G.size(), -1));
+        dist.assign(G.size(), -1);
+        dfs(0, -1, 0, G);
+
+        for (int k = 1; k < K; k++) {
+            for (int v = 0; v < G.size(); v++) {
+                if (parent[k - 1][v] < 0) parent[k][v] = -1;
+                else parent[k][v] = parent[k - 1][parent[k - 1][v]];
+            }
+        }
     }
-}
+
+    void dfs(int v, int p, int d, vector<vector<int>> &G) {  // root からの距離 と 2^0 個先の親を求める
+        dist[v] = d;
+        parent[0][v] = p;
+        for (int u: G[v]) {
+            if (u == p) continue;
+            dfs(u, v, d + 1, G);
+        }
+    }
+
+    int query(int u, int v) {  // u, v の共通祖先を返す
+        if (dist[u] < dist[v]) swap(u, v);  // u の方が深い
+
+        int K = parent.size();
+        for (int k = 0; k < K; k++) {
+            if ((dist[u] - dist[v]) >> k & 1) {
+                u = parent[k][u];
+            }
+        }
+
+        if (u == v) return u;
+
+        for (int k = K - 1; k >= 0; k--) {
+            if (parent[k][u] != parent[k][v]) {
+                u = parent[k][u];
+                v = parent[k][v];
+            }
+        }
+
+        return parent[0][u];
+    }
+
+    int get_dist(int u, int v) { // u, v の距離を返す
+        return dist[u] + dist[v] - 2 * dist[query(u, v)];
+    }
+
+};
 
 
 int main() {
@@ -25,6 +73,8 @@ int main() {
         G[B].push_back(A);
     }
 
+    LCA lca(G);
+
     int Q;
     cin >> Q;
 
@@ -38,19 +88,12 @@ int main() {
             V[k]--;
         }
 
-        vector<int> dp(N, 0);
-        for (int k = 0; k < K; k++) {
-            dp[V[k]] = 1;
+        if (K == 2) {
+            cout << lca.get_dist(V[0], V[1]) << endl;
+        } else if (K == 3) {
+            cout << (lca.get_dist(V[0], V[1]) + lca.get_dist(V[1], V[2]) + lca.get_dist(V[2], V[0])) / 2 << endl;
         }
 
-        dfs(0, -1, G, dp);
-
-        int ans = 0;
-        for (int i = 0; i < N; i++) {
-            if (dp[i] >= 1 && dp[i] != K) ans++;
-        }
-
-        cout << ans << endl;
     }
 
 }
