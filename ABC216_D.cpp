2645d85c6bbff1336531c8847bde87cd437d34c2
toona <amanou.toona@gmail.com>
2021-11-28 17:25:11 +0900

    AC: ABC216_D

--- ABC216_D.cpp
+++ ABC216_D.cpp
@@ -6,17 +6,50 @@ int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> A(M);
+    vector<queue<int>> A(M);
     for (int i = 0; i < M; i++) {
         int k;
         cin >> k;
-
         for (int j = 0; j < k; j++) {
             int a;
             cin >> a;
-            A[i].push_back(a);
+            a--;
+            A[i].push(a);
+        }
+    }    
+    
+    queue<int> q;
+    vector<vector<int>> mem(N, vector<int>());
+    for (int i = 0; i < M; i++) {
+        int u = A[i].front();
+        mem[u].push_back(i);
+        A[i].pop();
+
+        if (mem[u].size() != 2) continue;
+        for (int v: mem[u]) q.push(v);
+    }
+
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        if (A[u].size() == 0) continue;
+        int color = A[u].front();
+        mem[color].push_back(u);
+        A[u].pop();
+
+        if (mem[color].size() != 2) continue;
+        for (int v: mem[color]) q.push(v);
+    }
+
+
+
+    for (int i = 0; i < M; i++) {
+        if (A[i].size() != 0) {
+            cout << "No" << endl;
+            return 0;
         }
     }
 
-    
+    cout << "Yes" << endl;
 }
