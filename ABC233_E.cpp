c8da40404f1a61a88cb3fdd43d149744c489cd85
toona <amanou.toona@gmail.com>
2022-03-17 20:51:55 +0900

    AC

--- ABC233_E.cpp
+++ ABC233_E.cpp
@@ -6,26 +6,27 @@ int main() {
     string X;
     cin >> X;
 
-    vector<ll> N(X.size());
-    for (int i = 0; i < X.size(); i++) {
-        for (int j = 0; j <= i; j++) {
-            N[i - j] += X[j] - '0';
+    ll s = 0;
+    for (char x : X) {
+        s += x - '0';
+    }
+
+    string ans = "";
+    ll carry = 0;
+    for (int i = X.size() - 1;; i--) {
+        carry += s;
+        ans.push_back((carry % 10) + '0');
+
+        carry /= 10;
+        if (i >= 0) {
+            s -= X[i] - '0';
+        }
+        if (carry == 0 && s == 0) {
+            break;
         }
     }
 
-    ll carry = 0;
-    for (int i = 0; i < N.size(); i++) {
-        ll n = N[i];
-        N[i] = n % 10;
+    reverse(ans.begin(), ans.end());
 
-        carry = n / 10;
-        N[i + 1] += carry;
-    }
-
-    if (carry) cout << carry;
-    for (int i = N.size() - 1; i >= 0; i--) {
-        cout << N[i];
-    }
-
-    cout << endl;
+    cout << ans << endl;
 }
