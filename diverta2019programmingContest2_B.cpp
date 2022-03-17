afd0d701bdd32c95db9a52e9f4aff376134bcf05
toona <amanou.toona@gmail.com>
2022-03-18 06:39:11 +0900

    WA: 6AC 16WA

---
+++ diverta2019programmingContest2_B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> X(N), Y(N);
+    for (int i = 0; i < N; i++) cin >> X[i] >> Y[i];
+
+    map<pair<int, int>, int> m;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            m[make_pair(X[i] - X[j], Y[i] - Y[j])]++;
+        }
+    }
+
+    int ans = 0;
+    for (auto &[key, value] : m) {
+        ans = max(ans, value);
+    }
+    ans = N - ans;
+    cout << ans << endl;
+}
