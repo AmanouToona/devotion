75af0e15655b940609010682a96174619b468a27
toona <amanou.toona@gmail.com>
2022-07-20 22:09:21 +0900

    AC: ABC102_C

--- ABC102_C.py
+++ ABC102_C.py
@@ -5,12 +5,17 @@ def main():
     N = int(input())
     A = list(map(int, sys.stdin.readline().strip().split()))
 
-    for i in range(1, N + 1):
-        A[i - 1] -= i
-
+    A = [a - i for i, a in enumerate(A)]
     A.sort()
 
-    A_nodup = list(set(A))
+    # 中央値取得
+    A_mid = A[len(A) // 2]
+
+    ans = 0
+    for a in A:
+        ans += abs(a - A_mid)
+
+    print(ans)
 
     return
 
