70a8fa68e4bcbf421ee6d23e89fcb11e3914718e
toona <amanou.toona@gmail.com>
2022-07-24 11:41:41 +0900

    ToDo: ARC040 B

---
+++ ARC040_B.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    N, R = map(int, sys.stdin.readline().strip().split())
+    S = input()
+    S = [s for s in S]
+
+    return
+
+
+if __name__ == "__main__":
+    main()
