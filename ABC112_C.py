5e1693b9509b866438d96339bae0d22ca23c7774
toona <amanou.toona@gmail.com>
2022-07-28 02:39:31 +0900

    WA: 1wa  ABC 112 C

---
+++ ABC112_C.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def main():
+    N = int(input())
+
+    xyh = []
+    for _ in range(N):
+        xyh.append(tuple(map(int, sys.stdin.readline().strip().split())))
+
+    for cx in range(101):
+        for cy in range(101):
+            H = None
+            is_ans = True
+            for x, y, h in xyh:
+                if h == 0 and H is None:
+                    continue
+
+                if H is None:
+                    H = h + abs(x - cx) + abs(y - cy)
+                    continue
+
+                if h != max(H - abs(x - cx) - abs(y - cy), 0):
+                    is_ans = False
+                    break
+
+            if is_ans:
+                print(cx, cy, H)
+                return
+
+    return
+
+
+if __name__ == "__main__":
+    main()
