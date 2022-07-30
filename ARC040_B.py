65646de34ae75a8f69ebabfb1b2c46a0a1a5d17a
toona-laptop <amanou.toona@gmail.com>
2022-07-30 09:15:36 +0900

    RE: 1RE ARC040 B

--- ARC040_B.py
+++ ARC040_B.py
@@ -6,7 +6,34 @@ def main():
     S = input()
     S = [s for s in S]
 
-    return
+    # 一番右の塗られていないマスの位置
+    right_white = -1
+    for i, s in enumerate(S):
+        if s == ".":
+            right_white = i
+
+    # 最初から全部塗られている
+    if right_white == -1:
+        print(0)
+        return
+
+    ans = 0
+    pos = 0
+    while True:
+        # 今いるマスが塗装済みでないならば塗装する
+        if S[pos] == ".":
+            ans += 1
+            for i in range(R):
+                S[pos + i] = "o"
+        elif pos + R - 1 == right_white:
+            ans += 1
+
+        if pos + R - 1 == right_white:
+            print(ans)
+            return
+
+        pos += 1
+        ans += 1
 
 
 if __name__ == "__main__":
