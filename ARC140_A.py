81126e93003912a1c167e3334c8038435235be6b
toona-laptop <amanou.toona@gmail.com>
2022-08-26 19:58:52 +0900

    AC: ARC140_A

--- ARC140_A.py
+++ ARC140_A.py
@@ -1,9 +1,32 @@
 import sys
+from collections import defaultdict
 
 
 def main():
     N, K = map(int, sys.stdin.readline().strip().split())
     S = sys.stdin.readline().strip()
+
+    for res in range(1, N + 1):
+        if N % res != 0:
+            continue
+
+        to_change = 0
+        for start in range(res):
+            count = defaultdict(int)
+            n = start
+            while n < N:
+                count[S[n]] += 1
+                n += res
+
+            _to_chage = float("inf")
+            for _, val in count.items():
+                _to_chage = min(_to_chage, N // res - val)
+
+            to_change += _to_chage
+
+        if to_change <= K:
+            print(res)
+            return
 
 
 if __name__ == "__main__":
