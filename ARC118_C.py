fc165326c1d3c387db34cfbf17677a23ae8ac6c4
toona-laptop <amanou.toona@gmail.com>
2022-08-26 09:36:13 +0900

    AC: ARC118 C

--- ARC118_C.py
+++ ARC118_C.py
@@ -1,6 +1,23 @@
 def main():
     N = int(input())
 
+    ans = [15, 10, 6]
+
+    set_ans = set(ans)
+    for i in [15, 10, 6]:
+        if len(ans) >= N:
+            break
+        for j in range(2, 10000 // i):
+            if len(ans) >= N:
+                break
+
+            if i * j in set_ans:
+                continue
+
+            ans.append(i * j)
+            set_ans.add(i * j)
+    print(" ".join([str(a) for a in ans]))
+
 
 if __name__ == "__main__":
     main()
