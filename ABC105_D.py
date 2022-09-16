b83f55823d874e8513e4f822b1c3cf4d41a1f7ec
toona <amanou.toona@gmail.com>
2022-09-16 21:47:20 +0900

    AC: ABC105_D

--- ABC105_D.py
+++ ABC105_D.py
@@ -6,28 +6,18 @@ def main():
     N, M = map(int, sys.stdin.readline().strip().split())
     A = list(map(int, sys.stdin.readline().strip().split()))
 
-    all = sum(A) % M
-
-    mod_A = [a % M for a in A]
-
-    from_left = [0] + mod_A[:-1]
-    from_right = list(reversed(mod_A[1:] + [0]))
-
-    for i in range(len(from_left) - 1):
-        from_left[i + 1] += from_left[i]
-        from_left[i + 1] %= M
-    for i in range(len(from_right) - 1):
-        from_right[i + 1] += from_right[i]
-        from_right[i + 1] %= M
+    cusum = A.copy()
+    for i in range(len(cusum) - 1):
+        cusum[i + 1] += cusum[i]
 
     count = defaultdict(int)
-    for r in from_right:
-        count[r] += 1
+    count[0] += 1
 
     ans = 0
-    for i, left in enumerate(from_left):
-        ans += count[(all - left) % M]
-        count[from_right[-(i + 1)]] -= 1
+    for c in cusum:
+        c = c % M
+        ans += count[c]
+        count[c] += 1
 
     print(ans)
     return
