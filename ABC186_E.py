c978ae8b124dfea68cf0720407e5a853265fd350
toona-laptop <amanou.toona@gmail.com>
2022-09-25 13:16:46 +0900

    ToDo

---
+++ ABC186_E.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    T = int(input())
+
+    for _ in range(T):
+        N, S, K = map(int, sys.stdin.readline().strip().split())
+
+    return
+
+
+if __name__ == "__main_":
+    main()
