8bb11c5752dc30f24f6b5877e34979a375056937
toona <amanou.toona@gmail.com>
2023-06-16 18:52:03 +0900

    AC: ABC305 F

--- ABC305_F.py
+++ ABC305_F.py
@@ -4,17 +4,16 @@ sys.setrecursionlimit = 10**8
 
 
 def main():
-    N, M = map(int, sys.stdin.readline().strip().split())
+    N, _ = map(int, sys.stdin.readline().strip().split())
 
     num = [-1] * N
     num[0] = 0
     for n in range(1, 2 * N + 1):
-        i = sys.stdin.readline().strip()
+        i = input().strip()
         if i == "-1" or i == "OK":
             return
 
-        kv = list(map(int, i.split()))
-        v = kv[1:]
+        v = list(map(int, i.split()))[1:]
         v = [i - 1 for i in v]
 
         nxt = v[0]
@@ -25,6 +24,7 @@ def main():
         num[nxt] = n
 
         print(nxt + 1)
+    return
 
 
 if __name__ == "__main__":
